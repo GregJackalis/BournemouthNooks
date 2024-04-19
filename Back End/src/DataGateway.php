33bd83388b1de95b34015ba40687f92104ba8d76
TheJackal03 <gregorytsak3@gmail.com>
2024-04-19 13:51:01 +0300

    In case the table doess NOT exist in the database, the function that builds the table is called automatically within the class, with the appropriate data needed. Then a new function insertCSV() is called which sets up the sql query and everything else needed, in order to insert the data into the database. In addition, I also changed the other two functions that select the whole list or specific recources, so that they call the correct new table that is made and NOT the one I used before for debugging. With this setup, the API will now automatically create a table and insert data into it- if there isn't one- just by sending the same requests as before (get for the whole list etc).

--- Back End/src/DataGateway.php
+++ Back End/src/DataGateway.php
@@ -8,11 +8,18 @@ class DataGateway {
     public function __construct(DatabaseConnection $database, TableSetup $csvSetup) {
         $this->$conn = $database->getConnectionObj();
 
-        $isTable = $this->tableExists("datasets");
+        $exists = $this->tableExists("datasets");
 
-        if (!$isTable) {
-            $this->columnInfo = [$csvSetup->getColumnNames(), $csvSetup->getColumnTypes()];
+        if (!$exists) {
+            $this->columnInfo = [array_map('ltrim', $csvSetup->getColumnNames()), $csvSetup->getColumnTypes()];
             $this->buildTable();
+
+            $csvD = $csvSetup->getCSVData();
+
+            $this->insertCSV($csvD);
+
+        } else {
+            echo json_encode("Table to store data already exists!");
         }
 
     }
@@ -54,8 +61,41 @@ class DataGateway {
         return in_array($tableName, $tables);
     }
 
+    // NOT WORKING YET, NEED TO WORK ON THIS
+    private function insertCSV(array $data): void {
+        $columns = implode(', ',$this->columnInfo[0]);
+        // var_dump($columns);
+
+        // rtrim removes from the first paramter (a string), the character that is given on the second parameter FROM THE END of the string
+        // then in this case the string I'm giving it is the str_repeat function that will repeat the ? placeholder symbol for 5 times
+        // using the the count() function and the column names that can be found on the first position of the local columnInfo array-type variable
+        $params = rtrim(str_repeat('?, ', count($this->columnInfo[0])), ', ');
+
+
+        $sql = "INSERT INTO datasets ($columns) VALUES ($params)";
+
+        $stmt = $this->$conn->prepare($sql);
+    
+        foreach ($data as $row) {
+            if (count($row) !== count($this->columnInfo[0])) {
+                continue; // Skip if row size doesn't match column count
+            }
+    
+            $i = 1;
+            // Bind values to placeholders
+            foreach ($row as $value) {
+                // Note: Parameters are 1-indexed in PDO
+                $stmt->bindValue($i++, $value, PDO::PARAM_STR);
+            }
+
+            // Execute the statement
+            $stmt->execute();
+        }
+
+    }
+    
     public function getCol(): array {
-        $sql = "SELECT * FROM Courses";
+        $sql = "SELECT * FROM datasets";
 
         $stmt = $this->$conn->query($sql);
         
@@ -68,7 +108,7 @@ class DataGateway {
     }
 
     public function getSpecific(string $id): array {
-        $sql = "SELECT * FROM Courses WHERE id = ?";
+        $sql = "SELECT * FROM datasets WHERE id = ?";
 
         $stmt = $this->$conn->prepare($sql);
 
